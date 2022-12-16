580a4a85ce5b5df9c0dd40dce4d9fbb1d96408b0
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-16 11:44:29 -0600

    all but bonus mission

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
 
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?')  
@@ -19,24 +20,31 @@ export class AppComponent {
       this.height = 10000;
       this.width = 0;
       this.message = 'Shuttle in flight';
+      this.takeOffEnabled = false;
     }
   }
 
-  handleLanding() {
+  handleLanding(rocketImage) {
     alert("The shuttle is landing. Landing gear engaged.");
     this.color = 'green';
     this.height = 0;
     this.width = 0;
     this.message = "The shuttle has landed.";
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+    this.takeOffEnabled = true;
   }
 
-  handleAbort() {
+  handleAbort(rocketImage) {
     let result = window.confirm("Are you sure you want to abort?");
     if (result) {
       this.color = 'green';
       this.height = 0;
       this.width = 0;
       this.message = "Mission aborted.";
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0px';
+      this.takeOffEnabled = true;
     }
   }
 
@@ -64,5 +72,11 @@ export class AppComponent {
       rocketImage.style.bottom = movement;
       this.height -= 10000;
     }
+  }
+
+  warnAtLimits() {
+    if (this.width < -10000 || this.width > 160000 || this.height > 330000)
+      this.color = 'red';
+    else this.color = 'blue'
   }
 }
